ce809234982a9fb0225bb8ce6950450e3114e4a2
Ning0713 <jizhenning0713@163.com>
2026-02-28 20:04:53 +0800

    feat:add enemy move and animation.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+    protected Animator anim;
+
+    [Header("基本参数")]
+    public float normalSpeed;
+    public float chaseSpeed;
+    public float currentSpeed;
+    public Vector3 faceDir;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentSpeed = normalSpeed;
+    }
+    private void Update()
+    {
+        faceDir = new Vector3(-transform.localScale.x, 0, 0);
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    public virtual void Move()
+    {
+        rb.velocity = new Vector2(currentSpeed * faceDir.x * Time.deltaTime, rb.velocity.y);
+    }
+}
